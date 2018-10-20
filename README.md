8ad93fbd101620f3ac10546289912fdd5c6f0bac
sacreman <sacreman@gmail.com>
2018-10-20 21:14:52 +0700

    first working aks version

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # dolos
+
+Dolos is the spirit of trickery and guile. He is also a master at cunning deception, craftiness, and treachery. Very similar in many ways to most cloud marketing departments.
+
+# Usage
+
+## Azure
+
+Sign up to Azure and get your $200 of free credits.
+
+This process follows the [kubernetes walkthrough](https://docs.microsoft.com/en-us/azure/aks/kubernetes-walkthrough) documentation.
+
+`pip install --user requirements.txt`
+`az login`
+`az aks install-cli` # optional: only run if you want to install kubectl
+`while true; do ./aks.py >> log.txt; sleep 2; done`
+
+Leave this running for an hour or two and then grep the log.
+
+`grep -i "total create time taken" log.txt`
+`grep -i "total destroy time taken" log.txt`
+
+
+## Google
+
+Not currently implemented. Everyone knows this takes less than 3 minutes so I'll probably only do this if anyone wants proof.
+
+## Amazon
+
+This is currently dreadful. I have automation in Travis for this at work and it consistently takes 20 minutes.
+
+## Others
+
+Feel free to contribute a py file and some instructions in the readme.
---
+++ aks.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from azure.cli.core import get_default_cli
+import datetime
+import subprocess
+import sys
+import os
+import json
+import requests
+
+DIR_NAME = os.path.dirname(os.path.realpath(__file__))
+
+def az(command):
+    return get_default_cli().invoke(command)
+
+def log(message):
+    print("%s %s" % (datetime.datetime.now(), message))
+
+def status(code):
+    if code != 0:
+        log("Error")
+        sys.exit(1)
+    return True
+
+def cleanup():
+    log("Attempting to delete cluster")
+    destroy_start_time = datetime.datetime.now()
+    az(['group', 'delete', '--name', 'dolos', '--yes'])
+    destroy_end_time = datetime.datetime.now()
+    log("Cluster delete finished")
+    destroy_total_time = destroy_end_time - destroy_start_time
+    log("Total destroy time taken: %s" % str(destroy_total_time))
+    subprocess.check_output(['kubectl', 'config', 'unset', 'current-context'])
+    subprocess.check_output(['kubectl', 'config', 'unset', 'users.clusterUser_dolos_dolos'])
+    subprocess.check_output(['kubectl', 'config', 'delete-cluster', 'dolos'])
+    subprocess.check_output(['kubectl', 'config', 'delete-context', 'dolos'])
+
+#
+# Destroy Everything
+#
+cleanup()
+
+#
+# Create Everything
+#
+
+create_start_time = datetime.datetime.now()
+log("Starting test")
+
+log("Creating Resource Group")
+group_create = az(['group', 'create', '--name', 'dolos', '--location', 'eastus'])
+status(group_create)
+
+log("Creating the AKS cluster")
+aks_create = az(['aks', 'create', '--resource-group', 'dolos', '--name', 'dolos', '--node-count', '1', '--generate-ssh-keys'])
+status(aks_create)
+
+log("Getting cluster credentials")
+get_creds = az(['aks', 'get-credentials', '--resource-group', 'dolos', '--name', 'dolos'])
+
+log("Get Nodes")
+nodes = subprocess.check_output(['kubectl', 'get', 'nodes'])
+print(nodes)
+
+log("Applying Deployment")
+deploy_file = os.path.join(DIR_NAME, 'fixtures', 'azure-vote.yaml')
+deployment = subprocess.check_output(['kubectl', 'apply', '-f', deploy_file])
+print(deployment)
+
+log("Getting external IP")
+while True:
+    service = subprocess.check_output(['kubectl', 'get', 'service', 'azure-vote-front', '--output', 'json'])
+    external_ip = json.loads(service)['status']['loadBalancer']['ingress'][0]['ip']
+    if external_ip is not None:
+        break
+
+log("Getting web contents from %s" % external_ip)
+content = requests.get("http://" + external_ip).text
+
+with open(os.path.join(DIR_NAME, 'fixtures', 'azure-vote.html'), 'r') as testfile:
+    test_data = testfile.read()
+
+if test_data == content:
+    log("Test passed. Cluster complete.")
+    create_end_time = datetime.datetime.now()
+    create_total_time = create_end_time - create_start_time
+    log("Total create time taken: %s" % str(create_total_time))
+else:
+    log("Test failed. Exiting.")
+    sys.exit(1)
+
+#
+# Destroy Everything
+#
+
+cleanup()
