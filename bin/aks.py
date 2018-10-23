6bcb2a265971aa1f7ecb0959ebb061870100d338
Ahmed Sabbour <103856+sabbour@users.noreply.github.com>
2018-10-23 20:08:52 +0400

    Updated delete command

--- bin/aks.py
+++ bin/aks.py
@@ -66,7 +66,7 @@ def status(code):
 def cleanup():
     log("Attempting to delete cluster")
     destroy_start_time = datetime.datetime.now()
-    az(['aks', 'cluster' 'delete', '--name', 'dolos', '--group', 'dolos', '--yes'])
+    az(['aks', 'delete', '--name', 'dolos', '--group', 'dolos', '--yes'])
     destroy_end_time = datetime.datetime.now()
     log("Cluster delete finished")
     destroy_total_time = destroy_end_time - destroy_start_time
